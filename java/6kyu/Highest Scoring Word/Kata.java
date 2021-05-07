8b87791a0ee2d28947e1596046347bab6e804c3d
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2021-05-07 19:07:31 +0200

    Update Kata.java

--- java/6kyu/Highest Scoring Word/Kata.java
+++ java/6kyu/Highest Scoring Word/Kata.java
@@ -1,1 +1,23 @@
+public class Kata {
 
+    public static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String high(String s) {
+        String[] words = s.split(" ");
+        String max = "";
+        int maxPts = 0;
+        int pts;
+        for (String word : words) {
+            pts = 0;
+            for (int i = 0; i < word.length(); i++) {
+                pts += alphabet.indexOf(Character.toString(word.charAt(i))) + 1;
+            }
+            if (pts > maxPts) {
+                max = word;
+                maxPts = pts;
+            }
+        }
+        return max;
+    }
+
+}
